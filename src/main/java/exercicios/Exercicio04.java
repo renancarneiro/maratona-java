3084f692bd16e16ceed128ecf834226e669386d2
Renan carneiro <carneiro.renan5@gmail.com>
2026-02-11 23:04:14 -0300

    feat: exercicio 04

---
+++ src/main/java/exercicios/Exercicio04.java
@@ -0,0 +1,17 @@
+package org.example.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número inteiro");
+        int numero = scanner.nextInt();
+        int sucessor = numero + 1;
+        int antecessor = numero - 1;
+
+        System.out.println("Número digitado: "+numero);
+        System.out.println("O sucessor é: "+sucessor);
+        System.out.println("O antecessor é: "+antecessor);
+    }
+}
