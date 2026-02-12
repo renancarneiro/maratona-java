55aa4071cca19ff57c4f1b26441293ac0c92b456
renan.carneiro <renan.carneiro@tre-pa.jus.br>
2026-02-12 13:08:16 -0300

    feat: exercicio07

---
+++ src/main/java/exercicios/Exercicio07.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+    //7 - Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se ambos são VERDADEIRO ou FALSO.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite true ou false para o valor 1");
+        boolean valor1 = scanner.nextBoolean();
+        System.out.println("Digite true ou false para o valor 2");
+        boolean valor2 = scanner.nextBoolean();
+
+        System.out.println("Valor1: "+valor1);
+        System.out.println("Valor2: "+valor2);
+
+        boolean ambosVerdadeiro=valor1 & valor2;
+        boolean ambosFalso = !valor1 & !valor2;
+
+        if(ambosVerdadeiro){
+            System.out.println("Ambos valores VERDADEIRO");
+        }
+        if(ambosFalso){
+            System.out.println("Ambos valores FALSO");
+        }
+    }
+}
