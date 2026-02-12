38aeb7decb27501a726a46c4edcd899917331db9
Renan carneiro <carneiro.renan5@gmail.com>
2026-02-11 22:57:48 -0300

    feat: exercicio 03

---
+++ src/main/java/exercicios/Exercicio03.java
@@ -0,0 +1,26 @@
+package org.example.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor de A: ");
+        int a = scanner.nextInt();
+        System.out.println("Digite o valor de B");
+        int b = scanner.nextInt();
+
+        int resultado;
+        String mensagem;
+        if (a == b) {
+            resultado = a + b;
+            mensagem = "A soma dos valores é: ";
+        } else {
+            resultado = a * b;
+            mensagem = "A multiplicação dos valores é: ";
+        }
+        System.out.println(mensagem+resultado);
+
+    }
+}
