1d5cafa1d8e24aa29327a5972237c14f91667b76
Renan carneiro <carneiro.renan5@gmail.com>
2026-02-13 22:54:29 -0300

    feat: exercicio 17

---
+++ src/main/java/exercicios/Exercicio17.java
@@ -0,0 +1,17 @@
+package org.example.exercicios;
+
+import java.util.Scanner;
+
+//17 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
+//
+//Fórmula: C = (5 * ( F-32) / 9)
+public class Exercicio17 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a temperatura em Fahrenheit");
+        float temperatura = scanner.nextFloat();
+        float celcius = (5 * (temperatura-32) / 9);
+
+        System.out.println("A temperatura convertida de Fahrenheit para Celcius é: "+celcius);
+    }
+}
