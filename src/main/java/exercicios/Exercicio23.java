d9464ddb1ed3530624a9c97096ff46646441804c
renan.carneiro <renan.carneiro@tre-pa.jus.br>
2026-02-17 13:11:20 -0300

    feat: exercicio 23

---
+++ src/main/java/exercicios/Exercicio23.java
@@ -0,0 +1,7 @@
+package exercicios;
+//Faça um algoritmo que mostre um valor aleatório entre 0 e 100
+public class Exercicio23 {
+    public static void main(String[] args) {
+        System.out.println("O valor random é: "+ String.format("%.2f",Math.random() * 101));
+    }
+}
