448b7c67440ef54df30984212aadb225d5a16748
Renan carneiro <carneiro.renan5@gmail.com>
2026-03-26 07:02:27 -0300

    feat: aula herança pt1

---
+++ src/main/java/core/Hheranca/domain/Pessoa.java
@@ -0,0 +1,38 @@
+package core.Hheranca.domain;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public void imprime(){
+        System.out.println(this.nome);
+        System.out.println(this.cpf);
+        System.out.println(this.endereco.getRua());
+        System.out.println(this.endereco.getCep());
+    }
+}
---
+++ src/main/java/core/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,34 @@
+package core.Hheranca.test;
+
+import core.Hheranca.domain.Endereco;
+import core.Hheranca.domain.Funcionario;
+import core.Hheranca.domain.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco enderecoPessoa1 = new Endereco();
+        enderecoPessoa1.setCep("55502312");
+        enderecoPessoa1.setRua("Passagem bom jesus");
+
+        //Pessoa TEM Endereco -- associação
+        //Funcionario É uma pessoa -- Herança
+
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setCpf("02328832323");
+        pessoa1.setNome("Nome pessoa1");
+        pessoa1.setEndereco(enderecoPessoa1);
+        pessoa1.imprime();
+
+        Endereco enderecoFuncionario1 = new Endereco();
+        enderecoFuncionario1.setCep("222222222");
+        enderecoFuncionario1.setRua("Passagem funcionario");
+
+        System.out.println("------------------------------------");
+        Funcionario funcionario1 = new Funcionario();
+        funcionario1.setSalario(1600);
+        funcionario1.setCpf("000000000");
+        funcionario1.setNome("Funcionario");
+        funcionario1.setEndereco(enderecoFuncionario1);
+        funcionario1.imprime();
+    }
+}
