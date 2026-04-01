10ed0c0555a3a78979dfbd76a5e3ba546449f3e2
Renan carneiro <carneiro.renan5@gmail.com>
2026-03-31 21:27:52 -0300

    feat: modificador final

--- src/main/java/core/JmodificadorFinal/domain/Carro.java
+++ src/main/java/core/JmodificadorFinal/domain/Carro.java
@@ -1,9 +1,18 @@
 package core.JmodificadorFinal.domain;
 
 public class Carro {
-    public static final double VELOCIDADE_LIMITE;
-    static{
-        VELOCIDADE_LIMITE = 250;
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250;
+    public final Comprador COMPRADOR = new Comprador();
+//    static{
+//        VELOCIDADE_LIMITE = 250;
+//    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public final void imprimir(){
--- src/main/java/core/JmodificadorFinal/test/CarroTeste01.java
+++ src/main/java/core/JmodificadorFinal/test/CarroTeste01.java
@@ -5,9 +5,10 @@ import core.JmodificadorFinal.domain.Carro;
 public class CarroTeste01 {
     public static void main(String[] args) {
 //        Carro.VELOCIDADE_LIMITE = 200; como a variável estática está com final não é permitido fazer alteração no valor
-        System.out.println(Carro.VELOCIDADE_LIMITE);
-
         Carro carro = new Carro();
+        carro.COMPRADOR.setNome("Nome do comprador");
+        System.out.println(carro.COMPRADOR);
+
         carro.imprimir();
     }
 }
